f629150258d1b5bd335266371173972b15a34cb9
ly-collaps <75196225+ly-collaps@users.noreply.github.com>
2022-12-26 13:16:36 +0100

    Add files via upload

--- ITCP/SecondDay/THE CHALLENGE/SUBMIT YOUR SOLUTION/Second day/ProblemSixteen.py
+++ ITCP/SecondDay/THE CHALLENGE/SUBMIT YOUR SOLUTION/Second day/ProblemSixteen.py
@@ -1,3 +1,7 @@
 number = input("please enter a number: ")
-numberReversed = - int(number)
-print("the reversed  number of ",number , " is ", numberReversed)
+if number[0] == '-':
+    numberp= number[1::]
+    numberReversed = "-" + str(numberp[::-1])
+else:
+    numberReversed = number[::-1]
+print("the reversed  number of ", number , " is ", numberReversed)
